67f73bb2c8c8e13cc8b53e7c2e443f4e304cea8d
shrutipachpor810 <shrutipachpor810@gmail.com>
2025-06-26 23:56:12 +0530

    fix: add frontend URL to CORS config

--- stayfinder-backend/server.js
+++ stayfinder-backend/server.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: [
-    "http://localhost:5001",
     "https://stayfinder-frontend-sigma.vercel.app/"
   ],
   credentials: true
